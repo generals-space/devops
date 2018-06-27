fa971724017359d436ecf992870cbac9a3e7b305
general <generals.space@gmail.com>
2018-06-27 18:15:59 +0800

    将dockerfile迁移出去, 独立为dockerfile仓库

--- deploy/docker/dockerfile(明朔)/router.sh
+++
@@ -1,6 +0,0 @@
-## docker build --no-cache=true -f router.sh -t generals/router:1.0.0 .
-FROM generals/ttn:latest
-
-CMD source /etc/profile \
-    && cd $GOPATH/src/github.com/TheThingsNetwork/ttn \
-    && go run main.go router --config /etc/ttn/router/ttn.yml
